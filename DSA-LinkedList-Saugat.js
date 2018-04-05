459d657361f021768fc4daa1566625ae770a1e38
DhungelS <saugat.dhungel@gmail.com>
2018-04-05 13:17:57 -0400

    linked list

---
+++ DSA-LinkedList-Saugat.js
@@ -0,0 +1,17 @@
+const {_Node, LinkedList} = require('./linkedList')
+
+class LinkedList {
+    constructor() {
+            this.head = null;
+    }
+
+
+const main = () => {
+    class NewLinkedList {
+    constructor() {
+            this.head = null;
+    }
+ insertFirst(item){
+        this.head = new _Node(item, this.head);
+    }
+}
